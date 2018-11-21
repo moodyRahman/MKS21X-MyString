a3ed903c3bef569135154d5e367c54e590f5fbe7
Moody <mrahman01@stuy.edu>
2018-11-21 14:00:42 -0500

    day one init commit- mightve finished lab

---
+++ MyString.java
@@ -0,0 +1,43 @@
+public class MyString implements CharSequence{
+	private char[] data;
+
+	public MyString(CharSequence input){
+		data = new char[input.length()];
+		for (int x = 0; x < input.length(); x++){
+			data[x] = input.charAt(x);
+		}
+	}
+
+	public MyString(char[] input){
+		data = new char[input.length];
+		for (int x = 0; x < input.length; x++){
+			data[x] = input[x];
+		}
+	}
+
+	public char charAt(int index){
+		return data[index];
+	}
+
+	public MyString subSequence(int start, int end){
+		char[] output = new char[end - start];
+		int point = 0;
+		for (int x = start; x < end; x++){
+			output[point] = data[x];
+		}
+		return new MyString(output);
+	}
+
+	public int length(){
+		return data.length;
+	}
+
+	public String toString(){
+		String output = "";
+		for (int x = 0; x < data.length; x++){
+			output += data[x];
+		}
+		return output;
+	}
+
+}
