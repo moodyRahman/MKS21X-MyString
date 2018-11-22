034e10808e5b93db8e1c78e8ab9c96d89e105ccc
Moody <mrahman01@stuy.edu>
2018-11-22 18:10:53 -0500

    added documentation

--- MyString.java
+++ MyString.java
@@ -1,20 +1,37 @@
 public class MyString implements CharSequence, Comparable<CharSequence>{
 	private char[] data;
 
+	/**
+	 * Initializes MyString with some CharSequence (including a MyString)
+	 * @param input da any CharSequence lmao
+	 */
+
 	public MyString(CharSequence input){
+		//copies over CharSequence input into char[] data
 		data = new char[input.length()];
 		for (int x = 0; x < input.length(); x++){
 			data[x] = input.charAt(x);
 		}
 	}
 
+	/**
+	 * Alt constructor that takes in some 1-D array of chars
+	 * @param input 1-D char array
+	 */
 	public MyString(char[] input){
+		//allows for a direct char[] input into char[] data
 		data = new char[input.length];
 		for (int x = 0; x < input.length; x++){
 			data[x] = input[x];
 		}
 	}
 
+	/**
+	 * returns character at the input index
+	 * @param  index positive integer less then the length of the MyString,
+	 * 				 greater than 0
+	 * @return char at the specified location
+	 */
 	public char charAt(int index){
 		if (index < 0 || index >= data.length){
 			throw new IndexOutOfBoundsException();
@@ -22,6 +39,15 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
 		return data[index];
 	}
 
+	/**
+	 * slices the MyString starting from start (inclusive) until end (exclusive)
+	 * returns a new MyString from the specififed parameters
+	 * @throws IndexOutOfBoundsException if either start or end are out of bounds
+	 * 									 of the MyString
+	 * @param  start output MyString start location (inclusive)
+	 * @param  end   output MyString end location (exclusive)
+	 * @return       resultant output MyString
+	 */
 	public MyString subSequence(int start, int end){
 		if (start < 0 || end < 0 || start > end || end > data.length){
 			throw new IndexOutOfBoundsException();
@@ -35,10 +61,20 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
 		return new MyString(output);
 	}
 
+	/**
+	 * returns an int of the length of the MyString
+	 * @return int data length
+	 */
 	public int length(){
 		return data.length;
 	}
 
+	/**
+	 * compares the current instance of MyString to the CharSequence input
+	 *
+	 * @param  input CharSequence to be compared against
+	 * @return       1 if the two Objects are the swim, -1 if otherwise
+	 */
 	public int compareTo(CharSequence input){
 		for (int x = 0; x < data.length; x++){
 			if (data[x] != input.charAt(x)){
@@ -48,6 +84,10 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
 		return 1;
 	}
 
+	/**
+	 * String representation of MyString
+	 * @return String form of the MyString
+	 */
 	public String toString(){
 		return new String(data);
 	}
